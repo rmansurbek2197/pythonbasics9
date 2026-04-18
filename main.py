96c0926a0e2079a4beb2bb624120b7410ea62d20
rmansurbek2197 <rmansurbek2197@gmail.com>
2026-04-18 23:26:47 +0500

    Initial commit

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-for i in range(1, 101):
-    if i % 10 == 0:
-        print(i)
+def teskari_sozlarni_chiqar(matn):
+    sozlar = matn.split()
+    teskari_sozlar = sozlar[::-1]
+    for soz in teskari_sozlar:
+        print(soz)
+
+def asosiy_dastur():
+    matn = input("Matn kiriting: ")
+    print("Teskari so'zlar:")
+    teskari_sozlarni_chiqar(matn)
+
+asosiy_dastur()
