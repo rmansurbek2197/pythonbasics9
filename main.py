1142c898c8b8bc07dd4fb8fd3c0144412ce1b8a1
rmansurbek2197 <rmansurbek2197@gmail.com>
2026-04-18 23:34:38 +0500

    Initial commit

--- main.py
+++ main.py
@@ -3,68 +3,86 @@ class User:
         self.name = name
         self.email = email
 
+    def __str__(self):
+        return f"User: {self.name}, Email: {self.email}"
+
 class Project:
-    def __init__(self, name):
+    def __init__(self, name, description):
         self.name = name
+        self.description = description
         self.tasks = []
 
-class Task:
-    def __init__(self, name, status):
-        self.name = name
-        self.status = status
+    def add_task(self, task):
+        self.tasks.append(task)
+
+    def __str__(self):
+        return f"Project: {self.name}, Description: {self.description}"
 
 class Status:
     def __init__(self, name):
         self.name = name
 
-class TaskManagementApp:
+    def __str__(self):
+        return f"Status: {self.name}"
+
+class Task:
+    def __init__(self, name, description, status, user, project):
+        self.name = name
+        self.description = description
+        self.status = status
+        self.user = user
+        self.project = project
+
+    def __str__(self):
+        return f"Task: {self.name}, Description: {self.description}, Status: {self.status}, User: {self.user}, Project: {self.project}"
+
+class TaskManager:
     def __init__(self):
         self.users = []
         self.projects = []
         self.tasks = []
         self.statuses = []
 
-    def add_user(self, name, email):
-        self.users.append(User(name, email))
+    def add_user(self, user):
+        self.users.append(user)
 
-    def add_project(self, name):
-        self.projects.append(Project(name))
+    def add_project(self, project):
+        self.projects.append(project)
 
-    def add_task(self, name, status_name):
-        for status in self.statuses:
-            if status.name == status_name:
-                task = Task(name, status)
-                self.tasks.append(task)
-                for project in self.projects:
-                    project.tasks.append(task)
+    def add_task(self, task):
+        self.tasks.append(task)
 
-    def add_status(self, name):
-        self.statuses.append(Status(name))
+    def add_status(self, status):
+        self.statuses.append(status)
 
     def print_users(self):
         for user in self.users:
-            print(user.name, user.email)
+            print(user)
 
     def print_projects(self):
         for project in self.projects:
-            print(project.name)
+            print(project)
 
     def print_tasks(self):
         for task in self.tasks:
-            print(task.name, task.status.name)
+            print(task)
 
     def print_statuses(self):
         for status in self.statuses:
-            print(status.name)
+            print(status)
 
-app = TaskManagementApp()
-app.add_status("New")
-app.add_status("In Progress")
-app.add_status("Done")
-app.add_user("John Doe", "johndoe@example.com")
-app.add_project("Project 1")
-app.add_task("Task 1", "New")
-app.print_users()
-app.print_projects()
-app.print_tasks()
-app.print_statuses()
+user1 = User("John Doe", "johndoe@example.com")
+project1 = Project("Project 1", "This is project 1")
+status1 = Status("In Progress")
+task1 = Task("Task 1", "This is task 1", status1, user1, project1)
+task_manager = TaskManager()
+task_manager.add_user(user1)
+task_manager.add_project(project1)
+task_manager.add_task(task1)
+task_manager.add_status(status1)
+task_manager.print_users()
+task_manager.print_projects()
+task_manager.print_tasks()
+task_manager.print_statuses()
+project1.add_task(task1)
+print(project1.tasks[0])
